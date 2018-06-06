f513ed7c39e5a45abe16a2044cdb8ef2203c8244
Ryan Bernardino <r.bernardino17@gmail.com>
2018-06-06 12:41:55 +0800

    Adds network resources for the Azure (Websphere) network

--- Deploy-InfraNetworking.ps1
+++ Deploy-InfraNetworking.ps1
@@ -20,3 +20,24 @@ break
     -Name $resourceGroupName `
     -Force -Verbose
 #endregion
+
+#region Deploy the azure (websphere) network resource
+$location = 'East US'
+$resourceGroupName = 'azureNetwork'
+$resourceDeploymentName = 'azureNetwork-deployment'
+$templateFile = '.\AzRmTemplates\azure.network.json'
+$templateParameterFile = '.\AzRmTemplates\azure.network.parameters.json'
+
+New-AzureRmJsonTemplateDeployment `
+  -ResourceGroupName $resourceGroupName `
+  -DeploymentName $resourceDeploymentName `
+  -Location $location `
+  -Path $templateFile `
+  -TemplateParameterFile $templateParameterFile `
+  -Verbose -Force
+
+# Cleanup if needed
+Remove-AzureRmResourceGroup `
+  -Name $resourceGroupName `
+  -Force -Verbose
+#endregion
